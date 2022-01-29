969e71fd207e326285853074a69ba080dfde70d7
NeoMATRIX <neorog@hotmail.com>
2022-01-28 23:13:13 -0300

    Actualizacion

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,6 +1,6 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-// Crea una variable "string", puede contener lo que quieras:
+// Crea una variable "stricd cng", puede contener lo que quieras:
 const nuevaString = 'Holas';
 
 // Crea una variable numérica, puede ser cualquier número:
